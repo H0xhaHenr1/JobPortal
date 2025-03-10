e8b8f102c01a33858b7b48e4a6267a95c225bb91
H0xhaHenr1 <henrihoxha2016@gmail.com>
2025-03-10 11:21:45 +0100

    Update JobPostingResource.java

--- src/main/java/com/finalProjectLufthansa/JobPortal/resource/JobPostingResource.java
+++ src/main/java/com/finalProjectLufthansa/JobPortal/resource/JobPostingResource.java
@@ -5,6 +5,6 @@ public record JobPostingResource(
         String description,
         String requirements,
         String location,
-        Long employerId // Store only the employer's ID instead of the full User object
+        Long employerId 
 ) {
 }
